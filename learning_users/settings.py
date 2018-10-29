83929e354d184a3875b2597c42ced4f4e1ed1161
aayush10 <sharma.aayush42@gmail.com>
2018-10-29 16:39:24 +0530

    debug mode

--- learning_users/settings.py
+++ learning_users/settings.py
@@ -26,7 +26,7 @@ MEDIA_DIR  = os.path.join(BASE_DIR,'media')
 SECRET_KEY = '7f!mq+80yua2gpq_8jlk5*0&3_bvt_8-)+$-mb_or1gh_yjl8b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['aayusharma.pythonanywhere.com']
 
